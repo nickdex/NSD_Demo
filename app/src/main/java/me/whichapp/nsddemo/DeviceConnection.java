914c4ad54667109d77e1dcb3efb7f05953f055c0
Nikhil Warke <nikwarke@gmail.com>
2016-03-29 22:14:02 +0530

    NSD Helper Implemented Layout created Button onClick methods implemented

---
+++ app/src/main/java/me/whichapp/nsddemo/DeviceConnection.java
@@ -0,0 +1,19 @@
+package me.whichapp.nsddemo;
+
+/**
+ * Created by Dexter on 29-Mar-16.
+ */
+public class DeviceConnection
+{
+    private int localPort = -1;
+
+    public int getLocalPort()
+    {
+        return localPort;
+    }
+
+    public void setLocalPort(int localPort)
+    {
+        this.localPort = localPort;
+    }
+}
--- app/src/main/java/me/whichapp/nsddemo/MainActivity.java
+++ app/src/main/java/me/whichapp/nsddemo/MainActivity.java
@@ -2,6 +2,9 @@ package me.whichapp.nsddemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -11,9 +14,13 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity
 {
 
+    private static final String TAG = "MainActivity";
     TextView deviceName;
 
     ListView deviceList;
+
+    NSDHelper nsdHelper;
+    DeviceConnection deviceConnection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -24,6 +31,21 @@ public class MainActivity extends AppCompatActivity
         deviceName = (TextView) findViewById(R.id.device_name);
         deviceList = (ListView) findViewById(R.id.listView);
 
+        nsdHelper = new NSDHelper(this);
+        nsdHelper.initializeNsd();
+    }
 
+    public void registerService(View v)
+    {
+        // Register service
+        if(deviceConnection.getLocalPort() > -1) {
+            nsdHelper.registerService(deviceConnection.getLocalPort());
+        } else {
+            Log.d(TAG, "ServerSocket isn't bound.");
+        }
+    }
+
+    public void clickDiscover(View v) {
+        nsdHelper.discoverServices();
     }
 }
